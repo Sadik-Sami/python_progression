592519a3991022898f511a4bb70fa5256ef21de2
Sadik-Sami <miasimanto@gmail.com>
2025-08-21 01:31:33 +0600

    Added linear and binary seach

---
+++ Searching_Sorting/Binary Search.py
@@ -0,0 +1,19 @@
+def binary_search(arr, target):
+    size = len(arr)
+    start = 0
+    end = size - 1
+    while start < end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return mid  # Found the target
+        elif arr[mid] > target:
+            end = mid - 1
+        elif arr[mid] < target:
+            start = mid + 1
+    return -1
+
+
+sorted_list = [10, 23, 35, 45, 50, 70]
+target = 50
+result = binary_search(sorted_list, target)
+print(result)
